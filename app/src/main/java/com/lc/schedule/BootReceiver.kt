4a5349ffad2426f50b7f68b975431f0800104eb5
wjia21920-oss <wjia21920@gmail.com>
2026-08-27 23:34:28 +0800

    Create BootReceiver.kt

---
+++ app/src/main/java/com/lc/schedule/BootReceiver.kt
@@ -0,0 +1,11 @@
+package com.lc.schedule
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        NotificationHelper.scheduleDaily(context)
+    }
+}
